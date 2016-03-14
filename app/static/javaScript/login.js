122cfae213a560d4dc481463079ba9b92dac0eca
Falnic <dragosnicoara1@gmail.com>
2016-03-14 17:20:54 +0200

    Part4 Database without query commands(Play time)

--- app/static/javaScript/login.js
+++ app/static/javaScript/login.js
@@ -1,8 +1,8 @@
-function set_openid(openid, pr)
+function set_openid(openid, provider)
 {
     var u = openid.search('<username>');
     if (u != -1){
-        user = prompt('Enter your ' + pr + ' username:');
+        user = prompt('Enter your ' + provider + ' username:');
         openid = openid.substr(0, u) + user;
     }
     form = document.forms['login'];
